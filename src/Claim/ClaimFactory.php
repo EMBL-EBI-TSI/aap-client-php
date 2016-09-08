a70f0600c31b163eb6cd6ccdde89c63b6b6d70bc
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2016-09-08 12:29:34 +0100

    Added expected validity for each of the claims to ClaimFactory

--- src/Claim/ClaimFactory.php
+++ src/Claim/ClaimFactory.php
@@ -5,6 +5,15 @@ namespace Claim;
 class ClaimFactory
 {
 	public function generateClaims() {
+		$validityClaims = ClaimFactory::generateValidityClaims();
+		# Since array_colum doesn't let us keep the key, we have
+		# to use this convoluted way to get it back.
+		return array_filter(array_combine(
+		           array_keys($validityClaims),
+		           array_column($validityClaims, 0)
+		       ));
+	}
+	public function generateValidityClaims() {
 		$claims = [];
 
 		$canonic = ['iat' => time(),
@@ -14,45 +23,45 @@ class ClaimFactory
 		    'sub' => 'test',
 		];
 
-		$claims['Good token']             = $canonic;
+		$claims['Good token']             = array($canonic, true);
 
 		$tmp = $canonic;
 		$tmp['iat'] = time() - 3600;
 		$tmp['exp'] = time() - 1;
-		$claims['Expired token']          = $tmp;
+		$claims['Expired token']          = array($tmp, false);
 
 		$tmp = $canonic;
 		unset($tmp['iat']);
-		$claims['No issue time token']    = $tmp;
+		$claims['No issue time token']    = array($tmp, false);
 
 		$tmp = $canonic;
 		$tmp['iat'] = time() + 3600;
 		$tmp['exp'] = time() + 3601;
-		$claims['Too early token']        = $tmp;
+		$claims['Too early token']        = array($tmp, false);
 
 		$tmp = $canonic;
 		unset($tmp['exp']);
-		$claims['No expiration token']    = $tmp;
+		$claims['No expiration token']    = array($tmp, false);
 
 		$tmp = $canonic;
 		$tmp['iss'] = 'tsi.ebi.ac.uk';
-		$claims['Untrusted issuer token'] = $tmp;
+		$claims['Untrusted issuer token'] = array($tmp, true);
 
 		$tmp = $canonic;
 		unset($tmp['iss']);
-		$claims['No issuer token']        = $tmp;
+		$claims['No issuer token']        = array($tmp, true);
 
 		$tmp = $canonic;
 		$tmp['aud'] = 'portal.ebi.ac.uk';
-		$claims['Unknown audience token'] = $tmp;
+		$claims['Unknown audience token'] = array($tmp, false);
 
 		$tmp = $canonic;
 		unset($tmp['aud']);
-		$claims['No aud token']           = $tmp;
+		$claims['No aud token']           = array($tmp, false);
 
 		$tmp = $canonic;
 		unset($tmp['sub']);
-		$claims['No subject token']       = $tmp;
+		$claims['No subject token']       = array($tmp, false);
 
 		return $claims;
 	}
