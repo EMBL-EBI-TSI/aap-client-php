8ee3086a032103e4c8a02f9a960494ade955bd7e
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2016-12-02 15:09:20 +0000

    Added comment to warn about possible exception
    That could happen when trying to unserialize a (non) token

--- src/Token/TokenUnserializer.php
+++ src/Token/TokenUnserializer.php
@@ -11,20 +11,25 @@ class TokenUnserializer
 	private $loader;
 
 	public function __construct($filename) {
-	$this->key = JWKFactory::createFromCertificateFile(
-		$filename,
-		['use'=>'sig', 'alg'=>'RS256']
-	);
-	$this->loader = new Loader();
+		$this->key = JWKFactory::createFromCertificateFile(
+			$filename,
+			['use'=>'sig', 'alg'=>'RS256']
+		);
+		$this->loader = new Loader();
 	}
 
+	/**
+	 * Unserializes a token, if it really was a token
+	 * @return [JWT, int] JWT token and the signature that was verified with.
+	 * @throws InvalidArgumentException if argument is not an unserializable token.
+	 */
 	public function getToken($serialized_token) {
-	$token = $this->loader->loadAndVerifySignatureUsingKey(
-		$serialized_token,
-		$this->key,
-		['RS256'],
-		$signature_index
-	);
-	return [$token, $signature_index];
+		$token = $this->loader->loadAndVerifySignatureUsingKey(
+			$serialized_token,
+			$this->key,
+			['RS256'],
+			$signature_index
+		);
+		return [$token, $signature_index];
 	}
 }
