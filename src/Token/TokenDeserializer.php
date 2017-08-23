d1815468c0758f6d2eb10ba196de8bd84baa7e78
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2017-08-23 16:09:03 +0100

    New: token deserialization supports multiple certs

--- src/Token/TokenDeserializer.php
+++ src/Token/TokenDeserializer.php
@@ -4,19 +4,34 @@ namespace AAP\Token;
 
 use Jose\Loader;
 use Jose\Factory\JWKFactory;
+use Jose\Object\JWKSet;
 
 class TokenDeserializer
 {
-    private $key;
+    private $key_set;
     private $loader;
 
-    public function __construct($filename)
+    /**
+     * Constructor
+     * param $filenames: string | array(string) one or more x509 certificates locations
+     */
+    public function __construct($filenames)
     {
-        $this->key = JWKFactory::createFromCertificateFile(
-            $filename,
-            ['use'=>'sig', 'alg'=>'RS256']
-        );
+        $this->key_set = new JWKSet();
         $this->loader = new Loader();
+
+        if (is_string($filenames)) {
+            $filenames = [$filenames];
+        }
+
+        foreach ($filenames as $filename) {
+            $key = JWKFactory::createFromCertificateFile(
+                $filename,
+                ['use'=>'sig', 'alg'=>'RS256']
+            );
+            $this->key_set->addKey($key);
+        }
+
     }
 
     /**
@@ -26,9 +41,9 @@ class TokenDeserializer
      */
     public function getToken($serializedToken)
     {
-        $token = $this->loader->loadAndVerifySignatureUsingKey(
+        $token = $this->loader->loadAndVerifySignatureUsingKeySet(
             $serializedToken,
-            $this->key,
+            $this->key_set,
             ['RS256'],
             $signatureIndex
         );
